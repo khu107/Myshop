5da9620b7a3efcdb28346c54b85a969fa29024ad
metablog <khu10735@naver.com>
2023-10-27 02:55:21 +0900

    Navbar css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import MyCart from "./pages/MyCart";
+import NewProduct from "./pages/NewProduct";
+import NotFound from "./pages/NotFound";
+import ProductDetail from "./pages/ProductDetail";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "/", element: <Home /> },
+      { path: "/products", element: <AllProducts /> },
+      {
+        path: "/products/new",
+        element: <NewProduct />,
+      },
+      {
+        path: "/products/:id",
+        element: <ProductDetail />,
+      },
+      {
+        path: "/carts",
+        element: <MyCart />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
